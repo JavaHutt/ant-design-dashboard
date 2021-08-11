fda8197913c0d9bf4a541ff9ef91945e134a8540
JavaHutt <alxmer111@gmail.com>
2021-08-11 12:13:14 +0400

    eslint added

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,21 @@ import { Layout, Menu } from 'antd';
 const { Header, Content, Footer } = Layout;
 const menus = ['Weigh map', 'Countries', 'Affiliate articles', 'Proxy testing', 'Partner testing', 'Prices', '3rd Party Apps'];
 
-const App = () => {
-    return (
-        <div className="app">
-            <Layout className="layout">
-                <Header>
-                    <div className="logo" />
-                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                        {menus.map((item, i) => {
-                            const key = i + 1;
-                            return <Menu.Item key={key}>{item}</Menu.Item>;
-                        })}
-                    </Menu>
-                </Header>
-                <Content style={{ padding: '0 50px' }}>
-                    <div className="site-layout-content">Content</div>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}></Footer>
-            </Layout>,
-        </div>
-    );
-}
+const App = () => (
+    <div className="app">
+        <Layout className="layout">
+            <Header>
+                <div className="logo" />
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                    {menus.map((item, i) => <Menu.Item key={`abc${i + 1}`}>{item}</Menu.Item>)}
+                </Menu>
+            </Header>
+            <Content style={{ padding: '0 50px' }}>
+                <div className="site-layout-content">Content</div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }} />
+        </Layout>
+    </div>
+);
 
 export default App;
