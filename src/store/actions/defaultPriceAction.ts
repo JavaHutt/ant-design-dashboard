a40501e0d7450837ffbd56a7e5592aadcc0c90ce
JavaHutt <alxmer111@gmail.com>
2021-09-02 22:06:32 +0400

    api request interceptors

--- src/store/actions/defaultPriceAction.ts
+++ src/store/actions/defaultPriceAction.ts
@@ -1,7 +1,22 @@
 import { Dispatch } from 'redux';
+import { CognitoUserSession } from 'amazon-cognito-identity-js';
 import { DefaultPriceAction, DefaultPriceActionTypes } from '../types/defaultPrice';
 import { DefaultPrice } from '../../models/price';
+import store from '../index';
 import api from '../../api';
+
+// TODO not sure, maybe it should return async function...
+api.interceptors.request.use(req => {
+    const { user } = store.getState();
+    user.user!.getSession((error: any, session: CognitoUserSession) => {
+        if (error) console.log(error);
+        const token = session.getIdToken().getJwtToken();
+        req.headers = {
+            Authorization: `Bearer ${token}`,
+        };
+    });
+    return req;
+});
 
 export const fetchDefaultPrice = () => (
     async (dispatch: Dispatch<DefaultPriceAction>) => {
